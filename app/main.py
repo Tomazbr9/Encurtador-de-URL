fbf6c606e82947d605a63a7da6240926375718ae
Bruno Tomaz <tomazcraft@gmail.com>
2025-03-09 07:52:14 -0300

    Foi criada uma rota para a home da api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status, Response
 
 app = FastAPI()
 
 
+@app.get('/', status_code=status.HTTP_200_OK)
+async def home():
+    return Response(content='Sucesso', status_code=status.HTTP_200_OK)
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True)
