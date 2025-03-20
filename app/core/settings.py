d2edf53967b1b144ef287e27f06c8eb2a0d2bc2c
Bruno Tomaz <tomazcraft@gmail.com>
2025-03-20 20:46:23 -0300

    Foi criado uma variavel chamada 'TEMPLATE' para armazenar o caminho da pasta templates

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,7 +1,10 @@
 from sqlalchemy.orm import declarative_base
+from fastapi.templating import Jinja2Templates
 
 # Classe base para definições de classes declarativas
 Base = declarative_base()
 # URL do banco de dados
 DB_URL: str = 'sqlite:///sqlite.db'
+# Define o diretorio dos templates
+TEMPLATES = Jinja2Templates(directory='app/templates')
 
