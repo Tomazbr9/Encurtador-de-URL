3cd72cea80a6a31fb8462228f661e68042061e98
Bruno Tomaz <tomazcraft@gmail.com>
2025-03-28 00:02:34 -0300

    Foi criado uma função que pega o usuario atual

--- app/services/authentication_services.py
+++ app/services/authentication_services.py
@@ -1,12 +1,15 @@
-from fastapi import Depends, HTTPException, status, Request
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials
 from sqlalchemy.orm import Session
-from services.db_services import session_local
 from models.url_models import UserModel
+from services.db_services import session_local
 from passlib.context import CryptContext
+from core.settings import SECURITY
+from typing import Optional
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Função para criptografar senha
+# Função para criptografar senha do usuário
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
@@ -14,13 +17,26 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-# Função para verificar se o usuario está authenticado
-def get_current_user(request: Request) -> str:
-    user = request.cookies.get('user')
+def get_current_user(
+        credentials: HTTPBasicCredentials = Depends(SECURITY),
+        db: Session = Depends(session_local)
+    ) -> UserModel | HTTPException:
+    
+    user = db.query(UserModel).filter(UserModel.username == credentials.username).first()
 
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Usuário não authenticado'
+            detail='Usuário não encontrado'
         )
+    
     return user
+
+def get_current_user_optional():
+
+    try:
+        user = Depends(get_current_user)
+        return user
+    except HTTPException:
+        return None
+
